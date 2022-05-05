254711b81d7edf87126deb84b25e5b9f1ea5dd8e
Ricardo Gonçalves <ricardo@mytiki.com>
2022-05-05 10:58:21 -0300

    file rename

--- lib/info_carousel.dart
+++ lib/info_carousel.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'src/cards/model/info_carousel_card_model.dart';
+import 'src/cards/model/model.dart';
 
 import 'src/info_carousel_service.dart';
 
-export 'src/cards/model/info_carousel_card_model.dart';
-export 'src/cards/model/info_carousel_card_model.dart';
-export 'src/cards/model/info_carousel_card_model_content.dart';
-export 'src/cards/model/info_carousel_card_model_content_body.dart';
-export 'src/cards/model/info_carousel_card_model_content_cta.dart';
-export 'src/cards/model/info_carousel_card_model_content_icon.dart';
-export 'src/cards/model/info_carousel_card_model_content_text.dart';
-export 'src/cards/model/info_carousel_card_model_cover.dart';
-export 'src/cards/model/info_carousel_card_model_cover_header.dart';
-export 'src/cards/model/info_carousel_card_model_cover_header_share.dart';
+export 'src/cards/model/model.dart';
+export 'src/cards/model/content.dart';
+export 'src/cards/model/content_body.dart';
+export 'src/cards/model/content_cta.dart';
+export 'src/cards/model/content_icon.dart';
+export 'src/cards/model/content_text.dart';
+export 'src/cards/model/cover.dart';
+export 'src/cards/model/cover_header.dart';
+export 'src/cards/model/cover_header_share.dart';
 
 class InfoCarousel {
   late final InfoCarouselService _service;
